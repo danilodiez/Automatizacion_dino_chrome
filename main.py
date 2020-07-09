4c4b0941e2dbd7b99753a70c0b406fd90a100ddf
DaniloDiez <danilodiez1@hotmail.com>
2020-07-09 14:17:33 -0300

    salto correcto

--- main.py
+++ main.py
@@ -12,20 +12,35 @@ def click(key):
 
 def colision(data): 
     #colision para las aves
-    for i in range (530,560):
-        for j in range(80, 127):
-            if data[i, j] < 171:
+    for i in range (660, 720):
+        for j in range(480, 530):
+            if data[i, j] < 100:
                 click("down")
                 return
+    
     #colision para los cactus
-    for i in range(530,560):
-        for i in range(130,160):
-            if data[i, j] < 100:
+    for i in range(660, 720):
+        for j in range(370, 410):
+            if data[i, j] < 120:
                 click("up")
-                return
+                return 
 
     return
 
-    
+if __name__ == "__main__":
+    time.sleep(5)
+    click("up")
 
+while True:
+    imagen = ImageGrab.grab().convert('L')
+    data = imagen.load()
+    # Aca sacamos captura para ver la combinacion ideal de pixeles
+    '''for i in range(660, 720):
+        for j in range(370, 410):
+            data[i, j] = 0
+    imagen.show()
+    break'''
+    colision(data)
+    
+    
 
